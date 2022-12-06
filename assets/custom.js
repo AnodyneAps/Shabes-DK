ad44ec6dba56a2cf580662fc5a84c73c1c62d452
munir <munir@anodyne.dk>
2022-12-06 11:32:26 +0100

    Counter added and cart page total prices restyled

--- assets/custom.js
+++ assets/custom.js
@@ -111,145 +111,142 @@ $('a.ProductItem__ColorSwatchItem, .More--colors .HorizontalList__Item').on('cli
     }
   }
 
-  flkty.on( 'change', function( index ) {
-    var sliderOpen = this.slider.getElementsByClassName('text_area--open')[0];
-    if (sliderOpen) {
-      var button = sliderOpen.getElementsByClassName('text_area_open')[0];
-      if (button) {
-        button.click()
-      }
-    }
+  // flkty.on( 'change', function( index ) {
+  //   var sliderOpen = this.slider.getElementsByClassName('text_area--open')[0];
+  //   if (sliderOpen) {
+  //     var button = sliderOpen.getElementsByClassName('text_area_open')[0];
+  //     if (button) {
+  //       button.click()
+  //     }
+  //   }
     
-  });
-  // blackdays counter 
-  const closeBtn = document.querySelector(".close__btn");
-  const flipBar = document.querySelector(".flip-container");
-  const flipFlopBar = document.getElementById("flipFlop");
-  
-  window.addEventListener("scroll", () => {
+  // });
+  // Every day counter 
+const closeBtn = document.querySelector(".close__btn");
+const flipBar = document.querySelector(".flip-container");
+
+
+window.addEventListener("scroll", () => {
     let scroll = this.scrollY;
     if(scroll > 699){ flipBar.classList.add("fixed-bottom")
     }
     else if(scroll < 500) {flipBar.classList.remove("fixed-bottom")
   }
-    
   });
-  if(typeof closeBtn != 'undefined' && closeBtn ){
-  
-  closeBtn.addEventListener("click", (e)=> {
-    localStorage.setItem("className", "show-flip");
-    let classFromLocalStorage = localStorage.getItem("className");
-    if (!classFromLocalStorage){
-      flipBar.classList.add("show-flip")
+
+closeBtn.addEventListener("click", ()=>{
+	localStorage.setItem("className", "show-flip");
+	let classFromLocalStorage = localStorage.getItem("className");
+	if (!classFromLocalStorage){
+		flipBar.classList.add("show-flip")
+	}
+	flipBar.classList.remove("show-flip");
+});
+
+const checkCookie = () => {
+	let classFromLocalStorage = localStorage.getItem("className");
+	if(classFromLocalStorage == "show-flip") {
+		flipBar.classList.add("hide");
+		flipBar.classList.remove("show-flip");
+	} else {
+		flipBar.classList.add("show-flip");
+		flipBar.classList.remove("hide");
+	}
+}
+
+window.onload = () =>{
+	setTimeout(()=>{
+		checkCookie();
+	},1000)
+}
+
+
+// clear localStorage after some time 
+let  minute = 3; // to clear the localStorage after 1 minute
+               // (if someone want to clear after 1 hour multiply by extra 60 or if you want to add more minuttes simply add 10 infront of minute variable)
+let now = new Date().getTime();
+var setupTime = localStorage.getItem('setupTime');
+if (setupTime == null) {
+    localStorage.setItem('setupTime', now)
+} else {
+    if(now-setupTime > minute*60*1000) {
+       localStorage.removeItem("className");
+        localStorage.setItem('setupTime', now);
     }
-    flipBar.classList.remove("show-flip");
-  });
-  }
-  const checkCookie = () => {
-    let classFromLocalStorage = localStorage.getItem("className");
-    if(classFromLocalStorage == "show-flip") {
-      flipBar.classList.add("hide");
-      flipBar.classList.remove("show-flip");
-    } else {
-      flipBar.classList.add("show-flip");
-      flipBar.classList.remove("hide");
-    }
-  }
-  
-  window.onload = () =>{
-    setTimeout(()=>{
-      checkCookie();
-    },1000)
-  }
-  
-  
-  // clear localStorage after some time 
-  let  minute = 1; // to clear the localStorage after 1 minute
-                 // (if someone want to clear after 1 hour multiply by extra 60 or if you want to add more minuttes simply add 10 infront of minute variable)
-  let now = new Date().getTime();
-  var setupTime = localStorage.getItem('setupTime');
-  if (setupTime == null) {
-      localStorage.setItem('setupTime', now)
-  } else {
-      if(now-setupTime > minute*60*1000) {
-         localStorage.removeItem("className");
-          localStorage.setItem('setupTime', now);
-      }
-  }
-  
-  
-  const dateCollect = flipFlopBar.dataset.collectDate;
-  const dateStr = dateCollect.replace(' ', 'T');
-  const countToBlackDays = new Date( dateStr );
-  let previousTimeBetweenDates
-  let daysContainer = document.querySelector("[data-days]");
-  // let dayDots = document.querySelector("[data-dots]");
-  
-  
-  setInterval(() => {
-    const currentDate = new Date()
-    const timeBetweenDates = Math.ceil((countToBlackDays - currentDate) / 1000)
-    flipAllCards(timeBetweenDates)
-    previousTimeBetweenDates = timeBetweenDates
-    if (countToBlackDays< currentDate){
-      flipFlopBar.style.display = "none";
-    } 
-    }, 250)
-  
-  
-  function flipAllCards(time) {
-  
-    const days = Math.floor(time * 1000 / (1000 * 60 * 60 * 24));
-    const seconds = time % 60
-    const minutes = Math.floor(time / 60) % 60
-    const hours = Math.floor(time * 1000 / (1000 * 60 * 60))
-    const difhours = hours - days * 24
-      if(days < 1){
-      daysContainer.style.display = "none"
-      // dayDots.style.display = "none"
-      }
-    flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10))
-    flip(document.querySelector("[data-days-ones]"), days % 10)
-    flip(document.querySelector("[data-hour-tens]"), Math.floor(difhours / 10))
-    flip(document.querySelector("[data-hour-ones]"), difhours % 10)
-    flip(document.querySelector("[data-minute-tens]"), Math.floor(minutes / 10))
-    flip(document.querySelector("[data-minute-ones]"), minutes % 10)
-    flip(document.querySelector("[data-second-tens]"), Math.floor(seconds / 10))
-    flip(document.querySelector("[data-second-ones]"), seconds % 10)
-  }
-  
-  function flip(flipCard, newNumber){
-  
-    const topHalf = flipCard.querySelector(".top");
-    const startNumber = parseInt(topHalf.textContent);
-    if(newNumber === startNumber) return 
-  
-    const bottomHalf = flipCard.querySelector(".bottom");
-    const topFlip = document.createElement("div");
-    topFlip.classList.add("top-flip");
-    const bottomFlip = document.createElement("div");
-    bottomFlip.classList.add("bottom-flip");
-  
-  
-  topHalf.textContent = startNumber;
-  bottomHalf.textContent = startNumber;
-  topFlip.textContent = startNumber;
-  bottomFlip.textContent = newNumber;
-  
-  
-  
-  topFlip.addEventListener("animationstart", e =>{
-    topHalf.textContent = newNumber;
-  })
-  topFlip.addEventListener("animationend", e =>{
-    topFlip.remove();
-  })
-  
-  bottomFlip.addEventListener("animationend", e =>{
-    bottomHalf.textContent = newNumber;
-    bottomFlip.remove();
-  })
-  
-  flipCard.append(topFlip, bottomFlip);
-  }
-  
+}
+
+setInterval(() => {
+	//set day hours
+	const d = new Date();
+	const setTimeOfDay = d.getHours();
+	//set day hours
+	let	date1 = new Date().setHours(08,59,59) 
+	let date2 =  new Date().setHours(14,59,59) 
+	let date3 = new Date().setHours(32,59,59) 
+
+  const currentDate = d;
+  const timeBetweenDates1 = Math.ceil(( date1 - currentDate ) / 1000)
+	const timeBetweenDates2 = Math.ceil(( date2 - currentDate ) / 1000);
+	const timeBetweenDates3 = Math.ceil(( date3 - currentDate ) / 1000)
+			if(setTimeOfDay >= 0 && setTimeOfDay < 9){
+				if(timeBetweenDates1 < 0) return
+				flipAllCards(timeBetweenDates1)
+			}
+			else if(setTimeOfDay >= 9 && setTimeOfDay < 15){
+				if(timeBetweenDates2 < 0) return
+				flipAllCards(timeBetweenDates2)
+			}
+			else{
+				if(timeBetweenDates3 < 0) return
+				flipAllCards(timeBetweenDates3)
+			}
+	}, 1000)
+
+
+function flipAllCards(time) {
+  const seconds = time % 60
+  const minutes = Math.floor(time / 60) % 60
+  const hours = Math.floor(time / 3600)
+
+  flip(document.querySelector("[data-hour-tens]"), Math.floor(hours / 10))
+  flip(document.querySelector("[data-hour-ones]"), hours % 10)
+  flip(document.querySelector("[data-minute-tens]"), Math.floor(minutes / 10))
+  flip(document.querySelector("[data-minute-ones]"), minutes % 10)
+  flip(document.querySelector("[data-second-tens]"), Math.floor(seconds / 10))
+  flip(document.querySelector("[data-second-ones]"), seconds % 10)
+}
+
+function flip(flipCard, newNumber){
+
+	const topHalf = flipCard.querySelector(".top");
+	const startNumber = parseInt(topHalf.textContent);
+	if(newNumber === startNumber) return 
+
+	const bottomHalf = flipCard.querySelector(".bottom");
+	const topFlip = document.createElement("div");
+	topFlip.classList.add("top-flip");
+	const bottomFlip = document.createElement("div");
+	bottomFlip.classList.add("bottom-flip");
+
+
+topHalf.textContent = startNumber;
+bottomHalf.textContent = startNumber;
+topFlip.textContent = startNumber;
+bottomFlip.textContent = newNumber;
+
+
+
+topFlip.addEventListener("animationstart", e =>{
+	topHalf.textContent = newNumber;
+})
+topFlip.addEventListener("animationend", e =>{
+	topFlip.remove();
+})
+
+bottomFlip.addEventListener("animationend", e =>{
+	bottomHalf.textContent = newNumber;
+	bottomFlip.remove();
+})
+
+flipCard.append(topFlip, bottomFlip);
+}
