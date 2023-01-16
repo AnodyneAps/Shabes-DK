924b9df96a563eba790d7439589bbf6c2c677a92
munir <munir@anodyne.dk>
2023-01-16 10:45:09 +0100

    product counter on frontpage and product pages added and the auto badge counter added too

--- assets/custom.js
+++ assets/custom.js
@@ -122,131 +122,175 @@ $('a.ProductItem__ColorSwatchItem, .More--colors .HorizontalList__Item').on('cli
     
   // });
   // Every day counter 
-const closeBtn = document.querySelector(".close__btn");
-const flipBar = document.querySelector(".flip-container");
-
-
-window.addEventListener("scroll", () => {
-    let scroll = this.scrollY;
-    if(scroll > 699){ flipBar.classList.add("fixed-bottom")
-    }
-    else if(scroll < 500) {flipBar.classList.remove("fixed-bottom")
+  const counter = document.getElementById("product__counter");
+  if(counter){
+  setInterval(() => {
+	  //set day hours
+	  const d = new Date();
+	  const setTimeOfDay = d.getHours();
+	  //set day hours
+	  let	date1 = new Date().setHours(08,59,59) 
+	  let date2 =  new Date().setHours(14,59,59) 
+	  let date3 = new Date().setHours(32,59,59) 
+  
+	const currentDate = d;
+	const timeBetweenDates1 = Math.ceil(( date1 - currentDate ) / 1000)
+	  const timeBetweenDates2 = Math.ceil(( date2 - currentDate ) / 1000);
+	  const timeBetweenDates3 = Math.ceil(( date3 - currentDate ) / 1000)
+			  if(setTimeOfDay >= 0 && setTimeOfDay < 9){
+				  if(timeBetweenDates1 < 0) return
+				  countTimer(timeBetweenDates1)
+			  }
+			  else if(setTimeOfDay >= 9 && setTimeOfDay < 15){
+				  if(timeBetweenDates2 < 0) return
+				  countTimer(timeBetweenDates2)
+			  }
+			  else{
+				  if(timeBetweenDates3 < 0) return
+				  countTimer(timeBetweenDates3)
+			  }
+	  }, 1000)
+  //add zeros
+  function addZero(num) {
+	  return ("0" + parseInt(num)).substr(-2);
   }
-  });
-
-closeBtn.addEventListener("click", ()=>{
-	localStorage.setItem("className", "show-flip");
-	let classFromLocalStorage = localStorage.getItem("className");
-	if (!classFromLocalStorage){
-		flipBar.classList.add("show-flip")
+  function countTimer(time){
+	  const seconds = addZero(time % 60)
+	  const minutes = addZero(Math.floor(time / 60) % 60)
+	  const hours = addZero(Math.floor(time / 3600))
+		counter.innerHTML = "<div class='inner'>"+hours+"<span class='dots'>:</span></div><div class='inner'>"+minutes+"<span class='dots'>:</span></div><div class='inner'>"+seconds+"</div>";
+  }
+  }
+  
+  
+  const closeBtn = document.querySelector(".close__btn");
+  const flipBar = document.querySelector(".flip-container");
+  
+  if(flipBar){
+  window.addEventListener("scroll", () => {
+	  let scroll = this.scrollY;
+	  if(scroll > 699){ flipBar.classList.add("fixed-bottom")
+	  }
+	  else if(scroll < 500) {flipBar.classList.remove("fixed-bottom")
 	}
-	flipBar.classList.remove("show-flip");
-});
-
-const checkCookie = () => {
-	let classFromLocalStorage = localStorage.getItem("className");
-	if(classFromLocalStorage == "show-flip") {
-		flipBar.classList.add("hide");
-		flipBar.classList.remove("show-flip");
-	} else {
-		flipBar.classList.add("show-flip");
-		flipBar.classList.remove("hide");
-	}
-}
-
-window.onload = () =>{
-	setTimeout(()=>{
-		checkCookie();
-	},1000)
-}
-
-
-// clear localStorage after some time 
-let  minute = 3; // to clear the localStorage after 1 minute
-               // (if someone want to clear after 1 hour multiply by extra 60 or if you want to add more minuttes simply add 10 infront of minute variable)
-let now = new Date().getTime();
-var setupTime = localStorage.getItem('setupTime');
-if (setupTime == null) {
-    localStorage.setItem('setupTime', now)
-} else {
-    if(now-setupTime > minute*60*1000) {
-       localStorage.removeItem("className");
-        localStorage.setItem('setupTime', now);
-    }
-}
-
-setInterval(() => {
-	//set day hours
-	const d = new Date();
-	const setTimeOfDay = d.getHours();
-	//set day hours
-	let	date1 = new Date().setHours(08,59,59) 
-	let date2 =  new Date().setHours(14,59,59) 
-	let date3 = new Date().setHours(32,59,59) 
-
-  const currentDate = d;
-  const timeBetweenDates1 = Math.ceil(( date1 - currentDate ) / 1000)
-	const timeBetweenDates2 = Math.ceil(( date2 - currentDate ) / 1000);
-	const timeBetweenDates3 = Math.ceil(( date3 - currentDate ) / 1000)
-			if(setTimeOfDay >= 0 && setTimeOfDay < 9){
-				if(timeBetweenDates1 < 0) return
-				flipAllCards(timeBetweenDates1)
-			}
-			else if(setTimeOfDay >= 9 && setTimeOfDay < 15){
-				if(timeBetweenDates2 < 0) return
-				flipAllCards(timeBetweenDates2)
-			}
-			else{
-				if(timeBetweenDates3 < 0) return
-				flipAllCards(timeBetweenDates3)
-			}
-	}, 1000)
-
-
-function flipAllCards(time) {
-  const seconds = time % 60
-  const minutes = Math.floor(time / 60) % 60
-  const hours = Math.floor(time / 3600)
-
-  flip(document.querySelector("[data-hour-tens]"), Math.floor(hours / 10))
-  flip(document.querySelector("[data-hour-ones]"), hours % 10)
-  flip(document.querySelector("[data-minute-tens]"), Math.floor(minutes / 10))
-  flip(document.querySelector("[data-minute-ones]"), minutes % 10)
-  flip(document.querySelector("[data-second-tens]"), Math.floor(seconds / 10))
-  flip(document.querySelector("[data-second-ones]"), seconds % 10)
-}
-
-function flip(flipCard, newNumber){
-
-	const topHalf = flipCard.querySelector(".top");
-	const startNumber = parseInt(topHalf.textContent);
-	if(newNumber === startNumber) return 
-
-	const bottomHalf = flipCard.querySelector(".bottom");
-	const topFlip = document.createElement("div");
-	topFlip.classList.add("top-flip");
-	const bottomFlip = document.createElement("div");
-	bottomFlip.classList.add("bottom-flip");
-
-
-topHalf.textContent = startNumber;
-bottomHalf.textContent = startNumber;
-topFlip.textContent = startNumber;
-bottomFlip.textContent = newNumber;
-
-
-
-topFlip.addEventListener("animationstart", e =>{
-	topHalf.textContent = newNumber;
-})
-topFlip.addEventListener("animationend", e =>{
-	topFlip.remove();
-})
-
-bottomFlip.addEventListener("animationend", e =>{
-	bottomHalf.textContent = newNumber;
-	bottomFlip.remove();
-})
-
-flipCard.append(topFlip, bottomFlip);
-}
+	});
+  if(closeBtn){
+	  closeBtn.addEventListener("click", ()=>{
+		  localStorage.setItem("className", "show-flip");
+		  let classFromLocalStorage = localStorage.getItem("className");
+		  if (!classFromLocalStorage){
+			  flipBar.classList.add("show-flip")
+		  }
+		  flipBar.classList.remove("show-flip");
+	  });
+  }
+  
+  
+  const checkCookie = () => {
+	  let classFromLocalStorage = localStorage.getItem("className");
+	  if(classFromLocalStorage == "show-flip") {
+		  flipBar.classList.add("hide");
+		  flipBar.classList.remove("show-flip");
+	  } else {
+		  flipBar.classList.add("show-flip");
+		  flipBar.classList.remove("hide");
+	  }
+  }
+  
+  window.onload = () =>{
+	  setTimeout(()=>{
+		  checkCookie();
+	  },1000)
+  }
+  
+  
+  // clear localStorage after some time 
+  let  minute = 3; // to clear the localStorage after 1 minute
+				 // (if someone want to clear after 1 hour multiply by extra 60 or if you want to add more minuttes simply add 10 infront of minute variable)
+  let now = new Date().getTime();
+  var setupTime = localStorage.getItem('setupTime');
+  if (setupTime == null) {
+	  localStorage.setItem('setupTime', now)
+  } else {
+	  if(now-setupTime > minute*60*1000) {
+		 localStorage.removeItem("className");
+		  localStorage.setItem('setupTime', now);
+	  }
+  }
+  
+  setInterval(() => {
+	  //set day hours
+	  const d = new Date();
+	  const setTimeOfDay = d.getHours();
+	  //set day hours
+	  let	date1 = new Date().setHours(08,59,59) 
+	  let date2 =  new Date().setHours(14,59,59) 
+	  let date3 = new Date().setHours(32,59,59) 
+  
+	const currentDate = d;
+	const timeBetweenDates1 = Math.ceil(( date1 - currentDate ) / 1000)
+	  const timeBetweenDates2 = Math.ceil(( date2 - currentDate ) / 1000);
+	  const timeBetweenDates3 = Math.ceil(( date3 - currentDate ) / 1000)
+			  if(setTimeOfDay >= 0 && setTimeOfDay < 9){
+				  if(timeBetweenDates1 < 0) return
+				  flipAllCards(timeBetweenDates1)
+			  }
+			  else if(setTimeOfDay >= 9 && setTimeOfDay < 15){
+				  if(timeBetweenDates2 < 0) return
+				  flipAllCards(timeBetweenDates2)
+			  }
+			  else{
+				  if(timeBetweenDates3 < 0) return
+				  flipAllCards(timeBetweenDates3)
+			  }
+	  }, 1000)
+  
+  
+  function flipAllCards(time) {
+	const seconds = time % 60
+	const minutes = Math.floor(time / 60) % 60
+	const hours = Math.floor(time / 3600)
+  
+	flip(document.querySelector("[data-hour-tens]"), Math.floor(hours / 10))
+	flip(document.querySelector("[data-hour-ones]"), hours % 10)
+	flip(document.querySelector("[data-minute-tens]"), Math.floor(minutes / 10))
+	flip(document.querySelector("[data-minute-ones]"), minutes % 10)
+	flip(document.querySelector("[data-second-tens]"), Math.floor(seconds / 10))
+	flip(document.querySelector("[data-second-ones]"), seconds % 10)
+  }
+  
+  function flip(flipCard, newNumber){
+  
+	  const topHalf = flipCard.querySelector(".top");
+	  const startNumber = parseInt(topHalf.textContent);
+	  if(newNumber === startNumber) return 
+  
+	  const bottomHalf = flipCard.querySelector(".bottom");
+	  const topFlip = document.createElement("div");
+	  topFlip.classList.add("top-flip");
+	  const bottomFlip = document.createElement("div");
+	  bottomFlip.classList.add("bottom-flip");
+  
+  
+  topHalf.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = newNumber;
+  
+  
+  
+  topFlip.addEventListener("animationstart", e =>{
+	  topHalf.textContent = newNumber;
+  })
+  topFlip.addEventListener("animationend", e =>{
+	  topFlip.remove();
+  })
+  
+  bottomFlip.addEventListener("animationend", e =>{
+	  bottomHalf.textContent = newNumber;
+	  bottomFlip.remove();
+  })
+  
+  flipCard.append(topFlip, bottomFlip);
+  }
+  }
